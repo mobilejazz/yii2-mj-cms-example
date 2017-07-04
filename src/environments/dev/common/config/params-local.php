3f52e1d94c93a52aed724a81d63db9a9a575143d
Miguel Ángel Martín <miguelbemartin@gmail.com>
2017-07-04 08:43:38 +0200

    Added default parameters to config the environments correctly

--- src/environments/dev/common/config/params-local.php
+++ src/environments/dev/common/config/params-local.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    'environment' => 'dev',
     'baseUrl' => '/',
     'adminBaseUrl' => '/admin'
 ];
--- src/environments/prod/common/config/params-local.php
+++ src/environments/prod/common/config/params-local.php
@@ -1,3 +1,6 @@
 <?php
 return [
+    'environment' => 'prod',
+    'baseUrl' => '/',
+    'adminBaseUrl' => '/admin'
 ];
